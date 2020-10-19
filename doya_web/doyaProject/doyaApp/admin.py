c41c624d279c337075f7561fc444e5d467bc9371
cromatical <lyh940830@gmail.com>
2020-10-19 15:18:39 +0900

    update
    로그인, 회원가입

--- doya_web/doyaProject/doyaApp/admin.py
+++ doya_web/doyaProject/doyaApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User_info, news_data
+
 
 # Register your models here.
+admin.site.register(User_info)
+admin.site.register(news_data)
+
--- doya_web/doyaProject/doyaApp/models.py
+++ doya_web/doyaProject/doyaApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class news_data(models.Model):
+    pass
+
+
+
+class User_info(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_email = models.CharField(max_length=50)
+    user_phone_num = models.CharField(max_length=50)
+    user_major = models.CharField(max_length=50)
+
+    user_id = models.CharField(max_length=50)
+    user_pw = models.CharField(max_length=50)
+    user_pw_check = models.CharField(max_length=50)
+
+
+
+
