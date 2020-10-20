4bde8275e7c10ce8834893024864a29a10b1c1c9
cromatical <lyh940830@gmail.com>
2020-10-20 21:53:39 +0900

    udpate
    업뎃합니다.

--- doya_web/doyaProject/doyaApp/admin.py
+++ doya_web/doyaProject/doyaApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User_info, news_data
+from .models import news_data, Profile
+from django.contrib.auth.models import User
 
 
 # Register your models here.
-admin.site.register(User_info)
+admin.site.register(Profile)
 admin.site.register(news_data)
 
