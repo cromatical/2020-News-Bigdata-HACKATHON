fa3098a51843ee3daa6e64511240aa56821bfc46
YoonHwan Lee <lyh940830@gmail.com>
2020-10-27 03:48:33 +0900

    update
    업데이트

--- doya_web/doyaProject/doyaApp/views.py
+++ doya_web/doyaProject/doyaApp/views.py
@@ -29,7 +29,7 @@ def home(request):
     user_major = None
     
 
-    data_path = r'C:\Users\KOH_AI\Documents\git_Dasktop\2020_News_Bigdata_HACKATHON\doya_web\doyaProject\doyaApp\data'
+    data_path = r'C:\Users\MyLaptop\Documents\Jupyter notebook\2020_News_Bigdata_HACKATHON\doya_web\doyaProject\doyaApp\data'
     major_fold = os.listdir(data_path)                    
 
     # data_path = r'C:\Users\MyLaptop\Desktop\개발\2020_News_Bigdata_HACKATHON\doya_web\doyaProject\doyaApp\data'
